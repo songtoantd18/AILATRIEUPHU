72782f8acfe43b25e4a3c333f2b7db3656cc24e2
songtoantd18@gmail.com <songtoan>
2024-04-22 09:08:47 +0700

    loading22

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   Link,
   useNavigate,
 } from "react-router-dom";
-import HomeScreen from "./page/HomeScreen";
+import HomeScreen from "./page/homeScreen";
 import LoginPage from "./page/loginPage";
 import NotFound from "./page/NotFound";
 import "../src/styles/index.css";
